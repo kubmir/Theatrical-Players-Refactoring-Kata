d46a875cf7384e9ce3340ed107c02c5d67c003b8
b3lix <erik.belko1@gmail.com>
2023-03-07 19:32:38 +0100

    Decompose main method to multiple methods

--- TheatricalPlayersRefactoringKata/StatementPrinter.cs
+++ TheatricalPlayersRefactoringKata/StatementPrinter.cs
@@ -13,32 +13,39 @@ namespace TheatricalPlayersRefactoringKata
             var result = string.Format("Statement for {0}\n", invoice.Customer);
             CultureInfo cultureInfo = new CultureInfo("en-US");
 
-            foreach(var perf in invoice.Performances) 
+            foreach (var perf in invoice.Performances)
             {
                 var play = plays[perf.PlayID];
-                var thisAmount = 0;
-                switch (play.Type) 
-                {
-                    case "tragedy":
-                        thisAmount = CalculateAmount(perf.Audience, 40000, 30, 1000);
-                        break;
-                    case "comedy":
-                        thisAmount = CalculateAmount(perf.Audience, 30000 + 300 * perf.Audience, 20, 500, 10000);
-                        break;
-                    default:
-                        throw new Exception("unknown type: " + play.Type);
-                }
-                // add volume credits
-                volumeCredits += Math.Max(perf.Audience - 30, 0);
-                // add extra credit for every ten comedy attendees
-                if ("comedy" == play.Type) volumeCredits += (int)Math.Floor((decimal)perf.Audience / 5);
+                var total_seats_in_perf = GetTotalAmountOfSeats(play, perf);
+
+                volumeCredits += CalculateVolumeCreditsAddition(play, perf.Audience);
 
                 // print line for this order
-                result += string.Format(cultureInfo, "  {0}: {1:C} ({2} seats)\n", play.Name, Convert.ToDecimal(thisAmount / 100), perf.Audience);
-                totalAmount += thisAmount;
+                result += string.Format(cultureInfo, "  {0}: {1:C} ({2} seats)\n", play.Name, Convert.ToDecimal(total_seats_in_perf / 100), perf.Audience);
+                totalAmount += total_seats_in_perf;
             }
             result += string.Format(cultureInfo, "Amount owed is {0:C}\n", Convert.ToDecimal(totalAmount / 100));
             result += string.Format("You earned {0} credits\n", volumeCredits);
+            return result;
+        }
+
+        private int GetTotalAmountOfSeats(Play play, Performance perf)
+        {
+            return play.Type switch
+            {
+                "tragedy" => CalculateAmount(perf.Audience, 40000, 30, 1000),
+                "comedy" => CalculateAmount(perf.Audience, 30000 + 300 * perf.Audience, 20, 500, 10000),
+                _ => throw new Exception("unknown type: " + play.Type),
+            };
+        }
+
+        private int CalculateVolumeCreditsAddition(Play play, int audience)
+        {
+            // add volume credits
+            var result = Math.Max(audience - 30, 0);
+            // add extra credit for every ten comedy attendees
+            if ("comedy" == play.Type) result += (int)Math.Floor((decimal)audience / 5);
+
             return result;
         }
 
